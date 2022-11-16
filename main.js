373c6e3ddb90e1618b95a206da77a1cc2d1c9a53
npereznyc <natalieperez84@gmail.com>
2022-11-16 13:11:15 -0500

    Updated timer so that it increments by minutes and seconds.

--- main.js
+++ main.js
@@ -3,31 +3,69 @@ let card= document.querySelectorAll('.singleCard')
 let timer=document.querySelector('#timerCount'); //timer number that will increment 
 const gameOver=document.createElement('h2')
 
-startBtn.addEventListener('click', function() {
-    let time=Number(timer.textContent);
-    function updateTimer(num) {
-        timer.textContent=num; 
-    }
 
-    function endGame () {
-        gameOver.innerText="Time is up, you've lost the game. Refresh the page to play again!"
-        return document.querySelector('#messageSection').appendChild(gameOver)
-    }
+window.onload=function (){
+    let seconds = 00;
+    let minutes = 00;
+    let appendMinutes = document.getElementById("minutes")
+    let appendSeconds = document.getElementById("seconds")
+    console.log(appendMinutes)
+    console.log(appendSeconds)
+    let Interval ;
 
-    interval=setInterval(()=> {
-        time+=1;
-        updateTimer(time);
+
+// startBtn.addEventListener('click', function()) {
+//     interval=setInterval(startTimer, 1000)()
         // if (timer>=5) {
         //     clearInterval(interval);
-        //     updateTimer(0);
+        //     // updateTimer(0);
         //     endGame();
         // } else {
         //     time+=1;
         //     updateTimer(time);
         // }
-    }, 1000) 
+    
+    
+startBtn.onclick=function(){
+    clearInterval(Interval);
+    Interval=setInterval(startTimer, 1000);
+}
+    
+function startTimer() {
+    seconds++;
+    if(seconds <=9) {
+        appendSeconds.innerHTML="0"+seconds;
+    }
+    if(seconds>9) {
+        appendSeconds.innerHTML = seconds;
+    }
+    if(seconds>59) {
+        console.log('minutes');
+        minutes++
+        appendMinutes.innerHTML="0"+minutes;
+        seconds=0;
+        appendSeconds.innerHTML="0"+0;
+    }
+    if(minutes>9) {
+        appendMinutes.innerHTML=minutes;
+    }
+}
+}
+    
+    // let time=Number(timer.textContent);
+    // function updateTimer(num) {
+    //     timer.textContent=num; 
+    // }
     // startTimer.disabled=true;
-})
+    function endGame () {
+        gameOver.innerText="Time is up, you've lost the game. Refresh the page to play again!"
+        return document.querySelector('#messageSection').appendChild(gameOver)
+   }
+// })
+// }
+
+
+
 
 card.forEach(function (cardClicked) {
     cardClicked.addEventListener('click', function() {

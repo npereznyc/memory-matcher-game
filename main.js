bfb4bee9860e13c9b27b3ddc11301a179b74999e
npereznyc <natalieperez84@gmail.com>
2022-11-16 12:31:29 -0500

    Start button now starts timer, incrementing up from 0.

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
 const body = document.querySelector('body')
 let card= document.querySelectorAll('.singleCard')
+let timer=document.querySelector('#timerCount'); //timer number that will increment 
+const gameOver=document.createElement('h2')
 
+startBtn.addEventListener('click', function() {
+    let time=Number(timer.textContent);
+    function updateTimer(num) {
+        timer.textContent=num; 
+    }
 
-card.forEach(function (i) {
-    i.addEventListener('click', function() {
-        console.log(i.className)
+    function endGame () {
+        gameOver.innerText="Time is up, you've lost the game. Refresh the page to play again!"
+        return document.querySelector('#messageSection').appendChild(gameOver)
+    }
+
+    interval=setInterval(()=> {
+        time+=1;
+        updateTimer(time);
+        // if (timer>=5) {
+        //     clearInterval(interval);
+        //     updateTimer(0);
+        //     endGame();
+        // } else {
+        //     time+=1;
+        //     updateTimer(time);
+        // }
+    }, 1000) 
+    // startTimer.disabled=true;
+})
+
+card.forEach(function (cardClicked) {
+    cardClicked.addEventListener('click', function() {
+        let card1=cardClicked.className
+        console.log(card1)
     })
 })
 

26ed13d83754559bbf4453b9502c8c48937ac63e
npereznyc <natalieperez84@gmail.com>
2022-11-18 16:10:59 -0500

    Board now cannot be clicked until the start button is clicked and the timer start.

--- main.js
+++ main.js
@@ -13,6 +13,69 @@ window.onload=function (){
 startBtn.addEventListener('click', function() {
     clearInterval(Interval); //prevents timer from incrementing faster if button is clicked again.
     Interval=setInterval(startTimer, 1000);
+
+    // const front = document.querySelector('#front')
+// const back = document.querySelector('#back')
+let allCards=document.querySelectorAll('.card')
+let card= document.querySelectorAll('.singleCard')
+let clickedCards=[]
+let startingScore=document.querySelector('#score')
+let score=Number(startingScore.textContent)
+// let cardFront=document.querySelectorAll('.cardFront')
+
+card.forEach(function (cardClicked) {
+    cardClicked.addEventListener('click', function() {
+        flip();
+        let card1=cardClicked.className
+        console.log(card1)
+        clickedCards.push(card1);
+        console.log(clickedCards)
+        if(clickedCards.length===2) {
+            checkMatch();
+            clickedCards=[];
+        }
+    })
+
+        function flip () {
+            allCards.forEach(function(cardFlipped) { 
+                cardFlipped.addEventListener('click', function() {
+                    cardFlipped.classList.add('flip');
+                console.log('card flipped');
+            })
+        })
+    }
+
+    //     function flipBack () {
+    //         allCards.forEach(function(cardFlipped) { 
+    //             cardFlipped.classList.remove('flip')
+    //             console.log('card flipped back')
+    //     })
+    // }
+
+    function updateScore(num) {
+        startingScore.textContent=num;
+        }
+    
+        function checkMatch() {
+        if(clickedCards[0] == clickedCards[1]) {
+            console.log("match!");
+            //keep cards flipped over
+            score+=1;
+            updateScore(score);
+            // clickedCards[0].removeEventListener('click', function(){
+            // })
+             if(score ==10){
+                win()
+                 console.log("You win!")
+             } 
+             }
+         else {
+            // flipBack();
+            console.log("no match")
+            //flip cards back
+        }
+    }
+})
 })
     
 function startTimer() {
@@ -48,69 +111,3 @@ youWin.innerText="You've matched all the cards! You win!"
 return document.querySelector('#messageSection').appendChild(youWin)
 }
 
-const front = document.querySelector('#front')
-const back = document.querySelector('#back')
-let allCards=document.querySelectorAll('.card')
-let card= document.querySelectorAll('.singleCard')
-let clickedCards=[]
-let startingScore=document.querySelector('#score')
-let score=Number(startingScore.textContent)
-let cardFront=document.querySelectorAll('.cardFront')
-
-card.forEach(function (cardClicked) {
-    cardClicked.addEventListener('click', function() {
-        flip();
-        let card1=cardClicked.className
-        console.log(card1)
-        clickedCards.push(card1);
-        console.log(clickedCards)
-        if(clickedCards.length===2) {
-            checkMatch();
-            clickedCards=[];
-        }
-    })
-
-        function flip () {
-            allCards.forEach(function(cardFlipped) { 
-                cardFlipped.addEventListener('click', function() {
-                    cardFlipped.classList.add('flip');
-                console.log('card flipped');
-            })
-            // let flippedCard=document.querySelectorAll('.card')
-            // // console.log(flippedCard)
-            // flippedCard.classList.add('flip')
-            // console.log('card flipped')
-        })
-    }
-
-        function flipBack () {
-            allCards.forEach(function(cardFlipped) { 
-                cardFlipped.classList.remove('flip')
-                console.log('card flipped back')
-        })
-    }
-
-    function updateScore(num) {
-        startingScore.textContent=num;
-        }
-    
-        function checkMatch() {
-        if(clickedCards[0] == clickedCards[1]) {
-            console.log("match!");
-            //keep cards flipped over
-            score+=1;
-            updateScore(score);
-            // clickedCards[0].removeEventListener('click', function(){
-            // })
-             if(score ==10){
-                win()
-                 console.log("You win!")
-             } 
-             }
-         else {
-            flipBack();
-            console.log("no match")
-            //flip cards back
-        }
-    }
-})

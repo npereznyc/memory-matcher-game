146b6d54f03c890fd3e64e81c353c6fba212a387
npereznyc <natalieperez84@gmail.com>
2022-11-20 13:21:12 -0500

    Got the cards to flip back when not matched, but it's flipping back all of the cards, not just the ones that were clicked.

--- main.js
+++ main.js
@@ -10,95 +10,121 @@ window.onload=function (){
     let displaySeconds = document.querySelector("#seconds")
     let Interval ;
     
-startBtn.addEventListener('click', function() {
-    clearInterval(Interval); //prevents timer from incrementing faster if button is clicked again.
-    Interval=setInterval(startTimer, 1000);
+    startBtn.addEventListener('click', function() {
+        clearInterval(Interval); //prevents timer from incrementing faster if button is clicked again.
+        Interval=setInterval(startTimer, 1000);
 
-    // const front = document.querySelector('#front')
-// const back = document.querySelector('#back')
-let allCards=document.querySelectorAll('.card')
-let card= document.querySelectorAll('.singleCard')
-let clickedCards=[]
-let startingScore=document.querySelector('#score')
-let score=Number(startingScore.textContent)
-// let cardFront=document.querySelectorAll('.cardFront')
 
-card.forEach(function (cardClicked) {
-    cardClicked.addEventListener('click', function() {
-        flip();
-        let card1=cardClicked.className
-        console.log(card1)
-        clickedCards.push(card1);
-        console.log(clickedCards)
-        if(clickedCards.length===2) {
+    let allCards=document.querySelectorAll('.card')
+    let cardFront=document.querySelectorAll('.cardFront')
+    let card= document.querySelectorAll('.singleCard')
+    let clickedCards=[]
+    let flippedCards=[]
+
+    allCards.forEach(function (cardClicked) {
+        cardClicked.addEventListener('click', function() {
+            // flip();
+            cardClicked.classList.add('flip');
+                console.log(cardClicked)
+            let card1=cardClicked.className
+                console.log(card1)
+            clickedCards.push(card1);
+                console.log(clickedCards)
+            if(clickedCards.length===2) {
             checkMatch();
             clickedCards=[];
-        }
+            }
+        })
     })
 
-        function flip () {
-            allCards.forEach(function(cardFlipped) { 
-                cardFlipped.addEventListener('click', function() {
-                    cardFlipped.classList.add('flip');
-                console.log('card flipped');
-            })
+    // card.forEach(function (cardClicked) {
+    //     cardClicked.addEventListener('click', function() {
+    //         flip();
+    //         let card1=cardClicked.className
+    //             // console.log(card1)
+    //         clickedCards.push(card1);
+    //             // console.log(clickedCards)
+    //         if(clickedCards.length===2) {
+    //         checkMatch();
+    //             // if (clickedCards[0]!==clickedCards[1]) {
+    //             //     cardFlipped.classList.remove('flip')
+    //             //     console.log('card flipped back')
+    //         clickedCards=[];
+    //         }
+    //     })
+    // })
+
+    
+    // function flip () {
+    //     allCards.forEach(function(cardFlipped) { 
+    //         cardFlipped.addEventListener('click', function() {
+    //             cardFlipped.classList.add('flip');
+    //             console.log(cardFlipped)
+    //         })
+    //     })
+    // }
+    //array of cards that have been flipped - if there's no match, loop through and flip them back
+    // function flipBack () {
+    //     allCards.forEach(function(cardFlipped) { 
+    //             cardFlipped.classList.remove('flip')
+    //                 console.log('card flipped back')
+    //     })
+    // }
+     
+    function flipBack () {
+        allCards.forEach(function(cardClicked) { 
+                cardClicked.classList.remove('flip')
+                    console.log('card flipped back')
         })
     }
 
-    //     function flipBack () {
-    //         allCards.forEach(function(cardFlipped) { 
-    //             cardFlipped.classList.remove('flip')
-    //             console.log('card flipped back')
-    //     })
-    // }
-
+    let startingScore=document.querySelector('#score')
+    let score=Number(startingScore.textContent)
+    
     function updateScore(num) {
         startingScore.textContent=num;
         }
     
-        function checkMatch() {
-        if(clickedCards[0] == clickedCards[1]) {
-            console.log("match!");
-            //keep cards flipped over
-            score+=1;
-            updateScore(score);
-            // clickedCards[0].removeEventListener('click', function(){
-            // })
-             if(score ==10){
-                win()
-                 console.log("You win!")
-             } 
-             }
-         else {
-            // flipBack();
+    function checkMatch() {
+    if(clickedCards[0] == clickedCards[1]) {
+        console.log("match!");
+        score+=1;
+        updateScore(score);
+        // clickedCards[0].removeEventListener('click', function(){
+        // })
+        if(score ==10){
+        win()
+        } 
+        } else {
+            setTimeout (flipBack, 1000);
             console.log("no match")
             //flip cards back
         }
     }
-})
-})
+    })
     
-function startTimer() {
-    seconds++;
-    if(seconds <=9) {
-        displaySeconds.innerHTML="0"+seconds;
+    
+    function startTimer() {
+        seconds++;
+        if(seconds <=9) {
+            displaySeconds.innerHTML="0"+seconds;
+        }
+        if(seconds>9) {
+            displaySeconds.innerHTML = seconds;
+        }
+        if(seconds>59) {
+            minutes++
+            displayMinutes.innerHTML="0"+minutes;
+            seconds=0;
+            displaySeconds.innerHTML="0"+0;
+        }
+        if(minutes>=2) {
+            clearInterval(Interval);
+            endGame();
+            displaySeconds.innerHTML="0"+0;
+            displayMinutes.innerHTML="0"+0;
+        }
     }
-    if(seconds>9) {
-        displaySeconds.innerHTML = seconds;
-    }
-    if(seconds>59) {
-        minutes++
-        displayMinutes.innerHTML="0"+minutes;
-        seconds=0;
-        displaySeconds.innerHTML="0"+0;
-    }
-    if(minutes>=2) {
-        clearInterval(Interval);
-        endGame();
-        displaySeconds.innerHTML="0"+0;
-        displayMinutes.innerHTML="0"+0;
-    }
-}
 }
 
 function endGame () {
@@ -107,7 +133,7 @@ function endGame () {
 }
 
 function win() {
-youWin.innerText="You've matched all the cards! You win!"
+youWin.innerText="You've matched all the cards! You win! Refresh the page to play again."
 return document.querySelector('#messageSection').appendChild(youWin)
 }
 
